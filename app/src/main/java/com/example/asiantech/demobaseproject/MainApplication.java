c484268163fa8e7a80a1a7c9e302f257c09558cd
phuongdn <phuongdn@asiantech.vn>
2017-05-20 22:55:05 +0700

    add first commit

---
+++ app/src/main/java/com/example/asiantech/demobaseproject/MainApplication.java
@@ -0,0 +1,10 @@
+package com.example.asiantech.demobaseproject;
+
+import android.app.Application;
+
+/**
+ * Copyright © 2017 AsianTech inc.
+ * Created by phuongdn on 20/05/2017.
+ */
+public class MainApplication extends Application {
+}
